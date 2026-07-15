6c89e4da4b5e0ae1288627d2269124ac6c514a3c
AbeloTales <abellopezmacias@gmail.com>
2026-07-15 01:08:55 -0500

    adicion de test para spi

---
+++ test_spi.py
@@ -0,0 +1,18 @@
+import spidev
+import time
+
+# Configuración SPI
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.mode = 0b00
+spi.max_speed_hz = 100000
+
+print("Enviando señales de prueba al SPI...")
+try:
+    while True:
+        # Enviamos un paquete de prueba constante
+        spi.xfer2([255, 150, 100]) 
+        print("Enviando: 255, 150, 100")
+        time.sleep(1) # Envía cada segundo
+except KeyboardInterrupt:
+    spi.close()
